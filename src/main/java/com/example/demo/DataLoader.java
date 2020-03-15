46b13facefec6cc4e4a9f2ef1e5a23d7f6e376bf
playrey2010 <playrey2010@gmail.com>
2020-03-14 20:26:10 -0400

    added DataLoader just for roles

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String...strings) throws Exception {
+        roleRepository.save(new Role("USER"));
+    }
+}
